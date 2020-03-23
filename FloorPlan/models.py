829d47645cd7715f4a63ab6088842db1bc2c95a5
Kat <kmneil2@gmail.com>
2020-03-23 15:00:47 -0400

    add Project model and fix organization

--- FloorPlan/models.py
+++ FloorPlan/models.py
@@ -3,8 +3,19 @@ from django.db import models
 from users.models import User
 
 
-# Create your models here.
-class Contractor(models.Model):
+
+class ProjectManager(models.Model):
+	first_name = models.CharField(max_length=30, blank=False)
+	last_name = models.CharField(max_length=30, blank=False)
+	company = models.CharField(max_length=100, blank=True)
+	email = models.EmailField(max_length=100, blank=False)
+	phone = PhoneField(max_length=9, blank=False)
+
+	def __str__(self):
+		return f'Name:{self.first_name} {self.last_name} Company:{self.company} Phone Number:{self.phone}'
+
+
+class TeamMember(models.Model):
     title = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     role = models.CharField(max_length=200)
@@ -14,16 +25,19 @@ class Contractor(models.Model):
         return self.role
 
 
-
-class ProjectManager(models.Model):
-	first_name = models.CharField(max_length=20, blank=False)
-	last_name = models.CharField(max_length=20, blank=False)
-	company = models.CharField(max_length=100, blank=True)
-	email = models.EmailField(max_length=100, blank=False)
-	phone = PhoneField(max_length=9, blank=False)
+class Project(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	address = models.CharField(max_length=400, blank=False)
+	lot_number = models.CharField(max_length=100, blank=True)
+	owner = models.ForeignKey(
+		ProjectManager, on_delete=models.CASCADE, related_name='projects')
 
 	def __str__(self):
-		return f'Name:{self.first_name} {self.last_name} Company:{self.company} Phone Number:{self.phone}'
+		return f'Address and/or Lot number:{self.address}, {self.lot_number}'
+
+
+
+
 
 
 
