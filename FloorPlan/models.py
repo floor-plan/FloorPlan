62c18002abde510bb0df0c5fe755a65b285dbfcf
Marsha <marshakshearin@gmail.com>
2020-03-23 13:52:10 -0400

    Contractor Model created

--- FloorPlan/models.py
+++ FloorPlan/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class Contractor(models.Model):
+    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=200)
+    join_project = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.role
