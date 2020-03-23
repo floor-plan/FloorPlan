c598775858fae7e7419648b808147b4359e6b3ee
odyssic <stevenbuddenjr@gmail.com>
2020-03-23 13:28:14 -0400

    index was broken from location changed. fixed

--- FloorPlan/views.py
+++ FloorPlan/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'core/index.html')
 
 def project(request, pk):
     pass
