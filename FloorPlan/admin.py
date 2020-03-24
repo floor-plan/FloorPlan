e43faae3da22bfc4499cbc5ee8c457bef5acae2a
odyssic <stevenbuddenjr@gmail.com>
2020-03-24 13:49:08 -0400

    register models in admin.py

--- FloorPlan/admin.py
+++ FloorPlan/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-# from .models import ProjectManager, TeamMember, Project, Role, Category, Task
+from .models import ProjectManager, TeamMember, Project, Role, Category, Task
 
-# admin.site.register(ProjectManager, TeamMember)
+admin.site.register(ProjectManager)
+admin.site.register(TeamMember)
+admin.site.register(Project)
+admin.site.register(Role)
+admin.site.register(Category)
+admin.site.register(Task)
 
 # Register your models here.
