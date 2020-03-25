05538027dbf15c8697c72eac83cb4bb714d80550
Kat <kmneil2@gmail.com>
2020-03-25 10:42:39 -0400

    url syntax

--- config/urls.py
+++ config/urls.py
@@ -10,14 +10,14 @@ urlpatterns = [
     path('', views.dashboard, name = 'dashboard'),
     path('project/<int:pk>', views.project, name='project'),
     path('new-project/', views.new_project, name='new_project'),
-    path('edit-project/<int:pk>', views.edit_project, name='edit_project'),
-    path('delete-project/<int:pk>', views.delete_project, name='delete-project'),
-    path('project/new-task/<int:pk>', views.new_task, name='new_task'),
-    path('edit-task/<int:pk>', views.edit_task, name='edit_task'),
-    path('delete-task/<int:pk>', views.delete_task, name='delete_task'),
-    path('new-team-member/<int:pk>', views.new_team_member, name='new_team_memeber'),
-    path('edit-team-member/<int:pk>', views.edit_team_member, name='edit_team_member'),
-    path('delete-team-member/<int:pk>', views.delete_team_member, name='delete_team_member'),
+    path('edit-project/<int:pk>/', views.edit_project, name='edit_project'),
+    path('delete-project/<int:pk>/', views.delete_project, name='delete_project'),
+    path('project/new-task/<int:pk>/', views.new_task, name='new_task'),
+    path('edit-task/<int:pk>/', views.edit_task, name='edit_task'),
+    path('delete-task/<int:pk>/', views.delete_task, name='delete_task'),
+    path('new-team-member/<int:pk>/', views.new_team_member, name='new_team_memeber'),
+    path('edit-team-member/<int:pk>/', views.edit_team_member, name='edit_team_member'),
+    path('delete-team-member/<int:pk>/', views.delete_team_member, name='delete_team_member'),
     
 ]
 
