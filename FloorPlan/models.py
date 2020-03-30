ac6272210931c82c6cd9017d84daa0697de67a85
Brandon <bmiller3822@gmail.com>
2020-03-30 17:54:04 -0400

    Tasks by user on dashboard, edit and delete categories, tasks per category on Project page.

--- FloorPlan/models.py
+++ FloorPlan/models.py
@@ -6,10 +6,21 @@ from django.utils import timezone
 # from django.contrib.auth.models import AbstractUser
 from users.models import User
 
+		
+class Project(models.Model):
+	name = models.CharField(max_length=100, blank=True) 
+	created_at = models.DateTimeField(auto_now_add=True)
+	address = models.CharField(max_length=400, blank=False)
+	lot_number = models.CharField(max_length=100, blank=True) 
+
+	def __str__(self):
+		return f'Address and/or Lot number:{self.address}, {self.lot_number}'
 
 class Category(models.Model):
 	ProjectCategory = models.TextChoices('ProjectCategory', 'PLUMBING ELECTRICAL MASONRY FRAMING ROOFING HOMEOWNER')
 	category = models.CharField(choices=ProjectCategory.choices, max_length=30, default='HOMEOWNER')
+	project = models.ForeignKey(
+		Project, on_delete=models.CASCADE, related_name="categories", default='')
 	
 	def __str__(self):
 		return self.category
@@ -18,16 +29,6 @@ class Category(models.Model):
 	# 	constraints = [models.UniqueConstraint(
 	# 	fields=['category', 'project'], name='unique_team'),
 	# 	]
-		
-class Project(models.Model):
-	name = models.CharField(max_length=100, blank=True) 
-	created_at = models.DateTimeField(auto_now_add=True)
-	address = models.CharField(max_length=400, blank=False)
-	lot_number = models.CharField(max_length=100, blank=True) 
-	category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categories', default='')
-
-	def __str__(self):
-		return f'Address and/or Lot number:{self.address}, {self.lot_number}'
     
 
 class Task(models.Model):
