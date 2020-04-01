1c63677d3e121dffe1c54e1b3f38cc9f9a17e5e6
Kat <kmneil2@gmail.com>
2020-04-01 16:47:42 -0400

    merge master

--- FloorPlan/views.py
+++ FloorPlan/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
