9f1048e656964d2f390e719220025c953a6e51a2
odyssic <stevenbuddenjr@gmail.com>
2020-04-04 07:41:17 -0400

    update tasks works

--- static/JS/main.js
+++ static/JS/main.js
@@ -1,35 +1,30 @@
 console.log("helloworld");
 
-updateButton = document.getElementById("update-button");
-
 function showUpdateButton() {
-    if (updatButton.classList.contains('hidden')) {
-        updateButton.classList.remove('hidden');
-        updateButton.classList.add('visible')
+    updateButton = document.getElementById("update-button");
+    updateButton.classList.remove("hidden");
+    updateButton.classList.add("visible");
+}
 
-        update
-        updateButton
-    }
+checkboxes = document.querySelectorAll(".checkbox");
 
-    checkboxes = document.querySelectorAll(".checkbox");
+function editThisTask(id) {
+    console.log("Where does this show up?");
+    console.log(id);
+    return fetch(`/complete_task/${id}/`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }
+    }).then(response => response.json());
+}
 
-    function editThisTask(id) {
-        console.log("Where does this show up?");
-        console.log(id);
-        return fetch(`/complete_task/${id}/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            }
-        }).then(response => response.json());
-    }
-
-    for (let checkbox of checkboxes) {
-        checkbox.addEventListener("change", function(event) {
-            event.preventDefault();
-            console.log("How many times does this happen?");
-            editThisTask(checkbox.id);
-            checkbox.classList.add("strikethrough");
-        });
-    }
+for (let checkbox of checkboxes) {
+    checkbox.addEventListener("change", function(event) {
+        event.preventDefault();
+        editThisTask(checkbox.id);
+        checkbox.classList.add("strikethrough");
+        showUpdateButton();
+    });
+}
